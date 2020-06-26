17b254ad01cf3c8046ad1fb0bb60b9b7d55a2c74
Daniel Singer <dannys430@yahoo.com>
2020-06-26 13:45:24 -0400

    added legend

--- frontend/app.js
+++ frontend/app.js
@@ -91,27 +91,46 @@ const svg = select('#container').append('svg')
     
     // const min = Math.min(...Object.values(counts))
     // const max = Math.max(...Object.values(counts))
-    const min = d3.min(countsArr, el => el.num)
-    const max = d3.max(countsArr, el => el.num)
-    const step = (max - min) / 30
+
+    // !!!
+    // const min = d3.min(countsArr, el => el.num)
+    // const max = d3.max(countsArr, el => el.num)
+    // const step = (max - min) / 30
     
+
+
     // const color = d3.scaleThreshold()
     //   .domain([.2, .9, 1.5, 3.5, 10, 20, 100, 300, 600])
     //   .range(["#f2f0f7", "#dadaeb", "#bcbddc", "#9e9ac8", "#756bb1", "#54278f", "#33125e", "#200a3d", "#140529" ]);
     
-    // const color = d3.scaleThreshold()
-    //   .domain([0, 1])
-    //   .range(["red", "white", "green"]);
+    // 60 steps
+    // const colorsScale = d3.scaleThreshold()
+    //   .domain([0, 50, 100, 150, 200, 250, 300, 350, 400, 450, 500, 550, 600, 650, 700, 750, 800, 850, 900, 950, 1000, 1050, 1100, 1150, 1200, 1250, 1300, 1350, 1400, 1450, 1500, 1550, 1600, 1650, 1700, 1750, 1800, 1850, 1900, 1950, 2000, 2050, 2100, 2150, 2200, 2250, 2300, 2350, 2400, 2450, 2500, 2550, 2600, 2650, 2700, 2750, 2800, 2850, 2900, 2950, 3000])
+    //   .range(['#FFFFFF', '#FBFBFA', '#F7F8F6', '#F3F5F2', '#EFF1EE', '#EBEEEA', '#E7EBE6', '#E3E7E2', '#DFE4DE', '#DBE1D9', '#D8DDD5', '#D4DAD1', '#D0D7CD', '#CCD3C9', '#C8D0C5', '#C4CDC1', '#C0C9BD', '#BCC6B8', '#B8C3B4', '#B4BFB0', '#B1BCAC', '#ADB9A8', '#A9B5A4', '#A5B2A0', '#A1AF9C', '#9DAB98', '#99A893', '#95A58F', '#91A18B', '#8D9E87', '#8A9B83', '#86987F', '#82947B', '#7E9177', '#7A8E72', '#768A6E', '#72876A', '#6E8466', '#6A8062', '#667D5E', '#637A5A', '#5F7656', '#5B7352', '#57704D', '#536C49', '#4F6945', '#4B6641', '#47623D', '#435F39', '#3F5C35', '#3C5831', '#38552C', '#345228', '#304E24', '#2C4B20', '#28481C', '#244418', '#204114', '#1C3E10', '#193B0C']);
+
+    // 30 steps
+    // const colorsScale = d3.scaleThreshold()
+    //   .domain([0, 100, 200, 300, 400, 500, 600, 700, 800, 900, 1000, 1100, 1200, 1300, 1400, 1500, 1600, 1700, 1800, 1900, 2000, 2100, 2200, 2300, 2400, 2500, 2600, 2700, 2800, 2900, 3000])
+    //   .range(['#FFFFFF', '#F7F8F6', '#EFF1EE', '#E7EBE6', '#DFE4DE', '#D8DDD5', '#D0D7CD', '#C8D0C5', '#C0C9BD', '#B8C3B4', '#B1BCAC', '#A9B5A4', '#A1AF9C', '#99A893', '#91A18B', '#8A9B83', '#82947B', '#7A8E72', '#72876A', '#6A8062', '#637A5A', '#5B7352', '#536C49', '#4B6641', '#435F39', '#3C5831', '#345228', '#2C4B20', '#244418', '#1C3E10']);
+
+    // const colorsScale = d3.scaleThreshold()
+    //   .domain([50, 100, 200, 500, 750, 1000, 1250, 1750, 2250, 2750])
+    //   .range(schemeGreens[9]);
 
     const colorsScale = d3.scaleThreshold()
-      .domain(range(min, max, step))
+      .domain([50, 100, 250, 500, 750, 1000, 1250, 1500])
       .range(schemeGreens[9]);
 
-    const colors = [];
+    // !!!
+    // const colorsScale = d3.scaleThreshold()
+    //   .domain(range(min, max, step))
+    //   .range(schemeGreens[9]);
 
-    for (let i = min; i <= max - 20; i += step) {
-      colors.push(i);
-    }
+
+    // const colors = [];
+    // for (let i = min; i <= max - 20; i += step) {
+    //   colors.push(i);
+    // }
     
     svg.append('g')
       .selectAll('path')
@@ -145,6 +164,74 @@ const svg = select('#container').append('svg')
     }).on('mouseout', () => {
       tooltip.classList.remove('show');
     });
+
+    const colors = [50, 100, 250, 500, 750, 1000, 1250, 1500]
+    
+    
+    const legendWidth = 300;
+    const legendHeight = 20;
+
+    const legendRectWidth = legendWidth / colors.length;
+    const legend = d3.select('g')
+      .append('svg')
+      .attr('x', 330)
+      .attr('y', 10)
+      .attr('width', legendWidth)
+      .attr('height', legendHeight)
+
+    const x = d3.scaleLinear()
+      .domain([50, 1500])
+      .rangeRound([500, 1000]);
+
+    legend.selectAll('rect')
+      .data(colors)
+      .enter()
+      .append('rect')
+      .attr('x', (_, i) => i * legendRectWidth)
+      .attr('y', 0)
+      .attr('width', legendRectWidth)
+      .attr('height', legendHeight)
+      .attr('fill', c => colorsScale(c))
+      // .call(d3.axisBottom(x)
+      // .tickSize(8)
+      // .tickFormat(x => Math.round(x) + '%')
+      // .tickValues(colorsScale.domain()))
+      // .select(".domain")
+      // .remove();
+
+    
+      // var newWidth = parseInt(select('rect').style("width")) * 4.1 + "px"
+
+      // .on('mouseover', () => {
+      //   // select('rect').attr('width', )
+      //   select('rect').style("transform", 'scale("1.3")')
+
+      //   // select('rect').attr("width", 400)
+
+      // }).on('mouseout', () => {
+      //   select('rect').attr('fill', 'initial')
+      // })
+  
+
+
+
+    //  svg
+    //  .enter()
+    //   .append("rect")
+    //   // .attr("height", 8)
+    //   // .attr("x", function (d) { return x(d[0]); })
+    //   // .attr("width", function (d) { return x(d[1]) - x(d[0]); })
+    //   // .attr("fill", function (d) { return color(d[0]); });
+    //   .attr('height', 50)
+    //   .attr('width', 500)
+    //   .attr('fill', 'blue')
+
+    // g.call(d3.axisBottom(x)
+    //   .tickSize(13)
+    //   .tickFormat(function (x) { return Math.round(x) + '%' })
+    //   .tickValues(color.domain()))
+    //   .select(".domain")
+    //   .remove();
 
   })
 
